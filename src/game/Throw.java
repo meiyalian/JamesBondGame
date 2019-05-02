c7de7a0a3f598751c8c53e3f74e22b047b1464f4
Meiya Lian <mlia0004@student.monash.edu>
2019-05-02 20:57:41 +1000

    Add stun powder and throw class, as well as the throwable interface

---
+++ src/game/Throw.java
@@ -0,0 +1,57 @@
+package game;
+
+import java.util.Random;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class Throw extends Action{
+	
+	private Actor actor;
+	private Actor subject;
+	private Throwable item;
+	
+	public Throw(Actor newActor, Actor newSubject, Throwable newItem) {
+		this.actor = newActor;
+		this.subject = newSubject;
+		this.item = newItem;
+		
+	}
+	
+	
+	
+	@Override
+	public String execute(Actor actor, GameMap map) {
+
+	
+		if (item.isHit()) {
+			int damage = ((WeaponItem) item).damage();
+			String result = actor + " " + ((WeaponItem) item).verb() + " " + subject + " for " + damage + " damage.";	
+			item.affect();
+			
+			return result;
+		}
+		
+		else {
+			return actor + " misses " + subject + ".";
+		}
+		
+		
+		
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String hotKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/game/Throwable.java
@@ -0,0 +1,7 @@
+package game;
+
+public interface Throwable {
+	public boolean isHit();
+	public void affect();
+	
+}
