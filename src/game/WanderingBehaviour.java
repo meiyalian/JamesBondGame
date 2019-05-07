e89e174c0734f0b3c7eb80857e53e44f5dd8d8ce
Meiya Lian <mlia0004@student.monash.edu>
2019-05-07 19:59:32 +1000

    add the wanderingBehaviour class

--- src/game/WanderingBehaviour.java
+++ src/game/WanderingBehaviour.java
@@ -1,18 +1,45 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.Exit;
 import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Location;
+import edu.monash.fit2099.engine.MoveActorAction;
 
 public class WanderingBehaviour implements ActionFactory {
 	
 	
+	private Random rand = new Random();
 	
 
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
-		// TODO Auto-generated method stub
+
+		ArrayList<Location> locations = new ArrayList<Location>();
+		
+		Location here = map.locationOf(actor);
+		
+		ArrayList<Exit> exits = (ArrayList<Exit>) here.getExits();
+		for (Exit exit : exits) {
+			Location destination = exit.getDestination();
+			if (destination.canActorEnter(actor)) {
+				locations.add(destination);
+			}
+			
+		}
+		if (locations.size()>0) {
+			int randomIndex = rand.nextInt( locations.size());
+			Location randomStep = locations.get(randomIndex);
+			return new MoveActorAction(randomStep,exits.get(randomIndex).getName());
+		}
+			
 		return null;
 	}
+	
+	
 
 }
