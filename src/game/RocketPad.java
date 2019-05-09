bd66d7a08ab8aec88898daba090624d335f73502
Zachary C K <zcol0001@student.monash.edu>
2019-05-10 02:32:14 +1000

    Modified the actor interface to work around actor active status limitation in the engine.

--- src/game/RocketPad.java
+++ src/game/RocketPad.java
@@ -21,16 +21,33 @@ public class RocketPad extends Ground{
 	
 	@Override
 	public Actions allowableActions(Actor actor, Location location, String direction) {
-		Actions actions = new Actions();
 		
-		for (Item item : actor.getInventory()) {
-			if (item instanceof RocketBody && this.hasBody == false)
-				actions.add(new GiveAction(actor, this.actorInterface, item));
-		
-			if (item instanceof RocketEngine && this.hasEngine == false)
-				actions.add(new GiveAction(actor, this.actorInterface, item));
+		for (Item item : this.actorInterface.getInventory()) {
+			if (item instanceof RocketBody) {
+				this.addBody();
+				this.actorInterface.removeItemFromInventory(item);
+			}
+			
+			if (item instanceof RocketEngine) {
+				this.addEngine();
+				this.actorInterface.removeItemFromInventory(item);
+			}
 		}
 		
+		Actions actions = new Actions();
+		
+		if (this.hasBody == false && this.hasEngine == false) {
+			for (Item item : actor.getInventory()) {
+				if (item instanceof RocketBody && this.hasBody == false)
+					actions.add(new GiveAction(actor, this.actorInterface, item));
+			
+				if (item instanceof RocketEngine && this.hasEngine == false)
+					actions.add(new GiveAction(actor, this.actorInterface, item));
+			}
+		}
+		else {
+			System.out.println("THE ROCKET HAS ALL THE PARTS, HAHAHA WE WIN!");
+		}
 		return actions;
 	}
 	
